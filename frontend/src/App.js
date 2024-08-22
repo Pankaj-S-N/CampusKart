8426707def4c6ef177f917dd65cc2816f5e4d73d
CaptainHades007 <arceusnfs@gmail.com>
2024-08-22 14:19:34 +0530

    Got a working linked navbar to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,22 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 import './App.css';
+import Navbar from './components/Navbar'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <div className='pages'>
           <Routes>
-            {/* <Route
+            <Route
               path='/'
               element={<Home />}
-            /> */}
+            />
             <Route
               path='/login'
               element={<Login />}
