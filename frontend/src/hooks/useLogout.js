29606d6ea7e32b58324180bb9b3303157e90b291
CaptainHades007 <arceusnfs@gmail.com>
2024-08-22 15:45:42 +0530

    added logout to navbar

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import { useAuthContext} from './useAuthContext'
+
+export const useLogout = () => {
+
+    // const [error, setError] = useState(null)
+    // const [isLoading, setIsLoading] = useState(null)
+    const { dispatch } = useAuthContext()
+    
+    const logut = async (email,password) => {
+        //remove user from storaeg
+        localStorage.removeItem('user')
+
+        //dispatch logout
+        dispatch({type: 'LOGOUT'})
+    }
+    //     setIsLoading(true)
+    //     setError(null)
+
+    //     const response = await fetch('/api/user/signup', {
+    //         method: 'POST',
+    //         headers: {'Content-type': 'application/json'},
+    //         body: JSON.stringify({email, password})
+    //     })
+
+    //     const json = await response.json()
+
+    //     if(!response.ok){
+    //         setIsLoading(false)
+    //         setError(json.error)
+    //     }
+    //     if(response.ok){
+    //         //saving the user to local storage
+    //         localStorage.setItem('user', JSON.stringify(json))
+
+    //         // update authContext
+    //         dispatch({type: 'LOGIN', payload: json})
+
+    //         setIsLoading(false)
+    //     }
+    // }
+    return logout
+}
