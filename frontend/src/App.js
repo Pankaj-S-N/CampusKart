6ce2f2a1d89fa575ecd0e781bbfbe819883f8835
CaptainHades007 <arceusnfs@gmail.com>
2024-08-22 12:05:24 +0530

    removed logo from app.js and signup typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
-import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -21,7 +20,7 @@ function App() {
               element={<Login />}
             />
             <Route
-              path='/singup'
+              path='/signup'
               element={<Signup />}
             />
           </Routes>
