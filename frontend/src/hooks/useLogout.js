1b63dc3e1b3e126152e5524a07dad464f341a401
CaptainHades007 <arceusnfs@gmail.com>
2024-08-22 15:49:17 +0530

    added useLogin hook

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -4,8 +4,7 @@ import { useAuthContext} from './useAuthContext'
 
 export const useLogout = () => {
 
-    // const [error, setError] = useState(null)
-    // const [isLoading, setIsLoading] = useState(null)
+    
     const { dispatch } = useAuthContext()
     
     const logout = async (email,password) => {
@@ -15,30 +14,6 @@ export const useLogout = () => {
         //dispatch logout
         dispatch({type: 'LOGOUT'})
     }
-    //     setIsLoading(true)
-    //     setError(null)
 
-    //     const response = await fetch('/api/user/signup', {
-    //         method: 'POST',
-    //         headers: {'Content-type': 'application/json'},
-    //         body: JSON.stringify({email, password})
-    //     })
-
-    //     const json = await response.json()
-
-    //     if(!response.ok){
-    //         setIsLoading(false)
-    //         setError(json.error)
-    //     }
-    //     if(response.ok){
-    //         //saving the user to local storage
-    //         localStorage.setItem('user', JSON.stringify(json))
-
-    //         // update authContext
-    //         dispatch({type: 'LOGIN', payload: json})
-
-    //         setIsLoading(false)
-    //     }
-    // }
     return logout
 }
