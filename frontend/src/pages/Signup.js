2a00ecd407afe5419fb553d3e2f2d2cd494566a1
CaptainHades007 <arceusnfs@gmail.com>
2024-08-22 12:08:18 +0530

    Added e in Login, Signup functions

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -5,7 +5,7 @@ const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
         console.log(email,password)
